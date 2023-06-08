967da74067b3ed2d56a90cb85c207d808a5f170e
Lizbeth Castro <lizbethcastro010@gmail.com>
2023-06-07 19:22:39 -0600

    Started routes up

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const apiRoutes = require("./routes/apiRoutes")
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("./public"));
+app.use("/api", apiRoutes)
 
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "public/notes.html"));
