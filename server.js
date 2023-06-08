c8d1f10725ec63a0d107f6475481ad76d2a2a649
Lizbeth Castro <lizbethcastro010@gmail.com>
2023-06-07 19:07:00 -0600

    Updated server file

--- server.js
+++ server.js
@@ -6,3 +6,13 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("./public"));
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.listen(PORT, () => console.log("Server running on PORT", PORT));
